7202e562923730fee936ff1c07b0bfc429a06c85
astrofra <astrofra@gmail.com>
2014-02-01 10:40:38 -0800

    - Added the Sega Saturn.

--- python/globals.py
+++ python/globals.py
@@ -3,10 +3,10 @@ SITE_ROOT			= '../www/'
 
 machine_list		= sorted([	'apple_2', 'oric','google_android', 'amstrad_cpc', 'nintendo_snes', 'pc_ms-dos', 'nintendo_gameboy', 'sega_mastersystem-gamegear',
 								'snk_neogeo_pocket', 'atari_lynx', 'msx', 'thomson_mo_to', 'atari_800xl', 'nec_pc_engine', 'arcade', 'vectrex', 'sinclair_ql',
-								'nintendo_nes', 'sinclair_spectrum', 'snk_neogeo', 'acorn_archimedes', 'cbm_64', 'cbm_amiga', 'sega_megadrive', 'atari_st'
+								'nintendo_nes', 'sinclair_spectrum', 'snk_neogeo', 'acorn_archimedes', 'cbm_64', 'cbm_amiga', 'sega_megadrive', 'sega_saturn', 'atari_st'
 								])
 
-machine_update_only = ['snk_neogeo_pocket']
+machine_update_only = ['sega_saturn']
 
 RESOURCES_ROOT		= 'resources'
 RESOURCES_FOLDERS	= ['assets', 'extern']
