95400438f632ef4568156ce1524b9a85ea501e1e
astrofra <astrofra@gmail.com>
2014-02-08 04:33:51 -0800

    - New machine (Atari 2600).

--- python/globals.py
+++ python/globals.py
@@ -4,10 +4,10 @@ SITE_ROOT			= '../www/'
 machine_list		= sorted([	'apple_2', 'oric','google_android', 'amstrad_cpc', 'nintendo_snes', 'pc_ms-dos', 'nintendo_gameboy', 'sega_mastersystem-gamegear',
 								'snk_neogeo_pocket', 'atari_st', 'atari_lynx', 'msx', 'thomson_mo_to', 'atari_800xl', 'nec_pc_engine', 'arcade', 'vectrex', 'sinclair_ql',
 								'nintendo_nes', 'sinclair_spectrum', 'snk_neogeo', 'acorn_archimedes', 'cbm_64', 'cbm_amiga', 'sega_megadrive', 'sega_saturn',
-								'wonderswan', 'ti-99', "sony_psx"
+								'wonderswan', 'ti-99', 'sony_psx', 'atari_2600'
 								])
 
-machine_update_only = ['sony_psx']
+machine_update_only = ['atari_2600']
 
 RESOURCES_ROOT		= 'resources'
 RESOURCES_FOLDERS	= ['assets', 'extern']
