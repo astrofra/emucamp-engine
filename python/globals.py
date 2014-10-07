fcea706abd42721fa62d6dd8de6375a54572ba4c
astrofra <astrofra@gmail.com>
2014-10-07 22:28:34 +0200

    - Additional machines (PS2).

--- python/globals.py
+++ python/globals.py
@@ -1,7 +1,7 @@
 SOURCE_ROOT			= 'sources/'
 SITE_ROOT			= '../www/'
 
-machine_list		= sorted([	'apple_2', 'oric','google_android', 'amstrad_cpc', 'nintendo_snes', 'pc_ms-dos', 'nintendo_gameboy', 'sega_mastersystem-gamegear',
+machine_list		= sorted([	'sony_ps2', 'apple_2', 'oric','google_android', 'amstrad_cpc', 'nintendo_snes', 'pc_ms-dos', 'nintendo_gameboy', 'sega_mastersystem-gamegear',
 								'snk_neogeo_pocket', 'atari_st', 'atari_lynx', 'msx', 'thomson_mo_to', 'atari_800xl', 'nec_pc_engine', 'arcade', 'vectrex', 'sinclair_ql',
 								'nintendo_nes', 'sinclair_spectrum', 'snk_neogeo', 'acorn_archimedes', 'cbm_64', 'cbm_amiga', 'sega_megadrive', 'sega_saturn',
 								'wonderswan', 'ti-99', 'sony_psx', 'atari_2600'
